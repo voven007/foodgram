cfc495c2e2ed653783a571497b4392ff9866e81d
voven007 <144550046+voven007@users.noreply.github.com>
2024-05-18 08:49:42 +0300

    v.3.7

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 
 MEDIA_URL = '/media/'
